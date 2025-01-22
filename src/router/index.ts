908fc1a13ff786beaa426d5fea4ce87d4c66c833
RitaYang <ritayang0811@gmail.com>
2025-01-22 22:50:37 +0800

    Fix:update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,33 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHashHistory("/RYwebsite/"),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  linkActiveClass: 'active',
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: () => import("../views/HomeView.vue"),
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
     },
     {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
     },
     {
-      path: "/projects",
-      name: "projects",
-      component: () => import("../views/ProjectsView.vue"),
+      path: '/projects',
+      name: 'projects',
+      component: () => import('../views/ProjectsView.vue'),
     },
     {
-      path: "/blog",
-      name: "blog",
-      component: () => import("../views/BlogView.vue"),
+      path: '/blog',
+      name: 'blog',
+      component: () => import('../views/BlogView.vue'),
     },
     {
-      path: "/contact",
-      name: "contact",
-      component: () => import("../views/ContactView.vue"),
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue'),
     },
   ],
 });
